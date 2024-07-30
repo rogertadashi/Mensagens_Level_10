245c9efd435b76794d714420bd2582b5ea419a42
rogertadashi <ryokomizo0@gmail.com>
2024-07-30 18:05:57 -0300

    Commit inicial do projeto Mensagens_Level_10

---
+++ UnitPrincipal.pas
@@ -0,0 +1,92 @@
+unit UnitPrincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button7: TButton;
+    Button8: TButton;
+    Button9: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+    procedure Button7Click(Sender: TObject);
+    procedure Button8Click(Sender: TObject);
+    procedure Button9Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  ShowMessage('Isso Foi uma Mensagem Simples.');
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  Application.MessageBox('Isso Foi um Alerta.','Atenção', MB_ICONEXCLAMATION+MB_OK);
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  Application.MessageBox('Isso Foi uma Informação','Informaçao', MB_ICONINFORMATION+MB_OK);
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+begin
+  Application.MessageBox('Aconteceu um Erro','Erro',MB_ICONERROR+MB_OK );
+end;
+
+procedure TForm1.Button5Click(Sender: TObject);
+begin
+  Application.MessageBox('Isso Aqui é uma Pergunta ?','Pergunta',+MB_ICONQUESTION	+MB_OK	);
+end;
+
+procedure TForm1.Button7Click(Sender: TObject);
+begin
+  Application.MessageBox('Que Horas são ?','Sim ou Não',MB_ICONQUESTION+MB_OKCANCEL )
+end;
+
+procedure TForm1.Button8Click(Sender: TObject);
+begin
+  if Application.MessageBox('Que Horas são ?','Sim ou Não',MB_ICONQUESTION+MB_YESNO) = mrYes then
+    begin
+      ShowMessage('Você Escolheu o Sim');
+    end
+  else
+    begin
+      ShowMessage('Você Escolheu o Não');
+    end;
+end;
+
+procedure TForm1.Button9Click(Sender: TObject);
+begin
+
+  if Application.MessageBox('Deseja Realmente Sair Do Sistema ?','Confirmação' , MB_ICONQUESTION+MB_YESNO) = mrYes then
+    begin
+        Application.Terminate;
+    end;
+
+end;
+
+end.
